8e5c6ebfb48fbdfa2f19065bc90718544d93e2b9
Sebastian Barajas <Desktop@seb.barajas@gmail.com>
2021-04-28 21:13:49 -0500

    updated schemas

--- schemas.sql
+++ schemas.sql
@@ -41,7 +41,7 @@ CREATE TABLE dept_emp(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,dept_no,from_date)
 );
 
 CREATE TABLE titles(
@@ -50,6 +50,6 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title,from_date)
 );
 
